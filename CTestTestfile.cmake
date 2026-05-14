ed30d7dbc4ff0b70ac1987244ff58bb8cf405231
Christian Schladetsch <christian.schladetsch@gmail.com>
2026-05-14 23:55:24 +1000

    Update CTest.

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /mnt/c/Users/chris/local/repos/CsharpFlow/AsmLang
-# Build directory: /mnt/c/Users/chris/local/repos/CsharpFlow/AsmLang
+# Source directory: /mnt/c/Users/chris/local/repos/AsmLang
+# Build directory: /mnt/c/Users/chris/local/repos/AsmLang
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
